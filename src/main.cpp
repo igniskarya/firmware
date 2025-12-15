85b4d3fd37582f8f7325dd8c5305adc13c1b3153
igniskarya <karyaignis@gmail.com>
2025-12-16 01:07:30 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -219,11 +219,11 @@ void begin_tft() {
  *********************************************************************/
 void boot_screen() {
     tft.setTextColor(bruceConfig.priColor, bruceConfig.bgColor);
-    tft.setTextSize(3);
+    tft.setTextSize(2);
     tft.drawPixel(0, 0, bruceConfig.bgColor);
     tft.drawCentreString("DOYA VIVO", tftWidth / 2, 10, 1);
-    tft.setTextSize(2);
-    tft.drawCentreString("MARAUDER V7", tftWidth / 2, 50, 1);
+    tft.setTextSize(1);
+    tft.drawCentreString("MARAUDER Mini", tftWidth / 2, 50, 1);
     tft.setTextSize(FM);
     tft.drawCentreString(
         "PREDATORY FIRMWARE", tftWidth / 2, tftHeight + 2, 1
