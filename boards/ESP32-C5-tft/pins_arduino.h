499ddd582ada21c51271cc7ced4e3a092d384ca1
igniskarya <karyaignis@gmail.com>
2026-01-16 08:05:28 +0700

    Update pins_arduino.h

--- boards/ESP32-C5-tft/pins_arduino.h
+++ boards/ESP32-C5-tft/pins_arduino.h
@@ -74,20 +74,20 @@ static const uint8_t LP_TX = 11;
 /* ---------------------   */
 // Setup for ST7789 170x320
 
-/*#define ST7789_DRIVER 1
+#define ST7789_DRIVER 1
 #define TFT_RGB_ORDER 0
  #define TFT_WIDTH 170
- #define TFT_HEIGHT 320*/
+ #define TFT_HEIGHT 320
 
 /* ---------------------   */
 // Setup for ST7735 128 x 169
 
-#define ST7735_DRIVER 1
+/*#define ST7735_DRIVER 1
 #define TFT_WIDTH 128
-#define TFT_HEIGHT 160
+#define TFT_HEIGHT 160*/
 //#define ST7735_GREENTAB3 1
 // // #define TFT_INVERSION_ON
-#define TFT_RGB_ORDER 0
+//#define TFT_RGB_ORDER 0
 
 /* ---------------------   */
 // Setup for ILI9341 320x240 (no touch)
@@ -102,7 +102,7 @@ static const uint8_t LP_TX = 11;
 #define TFT_BL 25
 #define TFT_RST 15
 #define TFT_DC 24
-#define TFT_MISO 2 // set to share SPI with other devices
+#define TFT_MISO -1 // set to share SPI with other devices
 #define TFT_MOSI 7
 #define TFT_SCLK 6
 #define TFT_CS 23
