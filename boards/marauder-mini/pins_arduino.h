f90ee05ec9df51bb187a6add50039f6311c1d108
igniskarya <karyaignis@gmail.com>
2026-02-04 03:36:22 +0700

    Update pins_arduino.h

--- boards/marauder-mini/pins_arduino.h
+++ boards/marauder-mini/pins_arduino.h
@@ -10,8 +10,8 @@
 static const uint8_t TX = 43;
 static const uint8_t RX = 44;
 
-static const uint8_t TXD2 = 1;
-static const uint8_t RXD2 = 2;
+// static const uint8_t TXD2 = 21;
+// static const uint8_t RXD2 = 22;
 
 static const uint8_t SDA = 13;
 static const uint8_t SCL = 15;
@@ -49,57 +49,75 @@ static const uint8_t G46 = 46;
 static const uint8_t ADC1 = 7;
 static const uint8_t ADC2 = 8;
 
-#define RGB_LED 21
+#define RGB_LED 32
 
-#define BAD_TX 21
-#define BAD_RX 22
+/*#define HAS_RGB_LED 1
+#define RGB_LED 2
+#define LED_TYPE WS2812B
+#define LED_ORDER GRB
+#define LED_TYPE_IS_RGBW 0
+#define LED_COUNT 16
+
+#define LED_COLOR_STEP 15*/
 
 // SERIAL (GPS) dedicated pins
-#define SERIAL_TX 21
-#define SERIAL_RX 22
-#define GPS_SERIAL_TX SERIAL_TX
-#define GPS_SERIAL_RX SERIAL_RX
+#define BAD_TX 1
+#define BAD_RX 3
+
+#define SERIAL_RX 3
+#define SERIAL_TX 1
+
 #define HAS_BTN 1
 #define SEL_BTN 34
 #define UP_BTN 36
 #define DW_BTN 35
 #define R_BTN 39
-#define L_BTN 13
+#define L_BTN 26
 #define HAS_5_BUTTONS
 #define BTN_ALIAS "\"Ok\""
 #define BTN_ACT LOW
 
-#define TXLED -1
+#define RXLED 16
+#define LED 17
 #define LED_ON HIGH
 #define LED_OFF LOW
 
-#define CC1101_GDO0_PIN -1
-#define CC1101_SS_PIN -1
+#define USE_CC1101_SPI 1
+#define CC1101_GDO2_PIN 22
+#define CC1101_GDO0_PIN 15
+#define CC1101_SS_PIN 27
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 
-#define NRF24_CE_PIN -1
-#define NRF24_SS_PIN -1
+#define USE_NRF24_SPI 1
+#define NRF24_CE_PIN 15
+#define NRF24_SS_PIN 27
 #define NRF24_MOSI_PIN SPI_MOSI_PIN
 #define NRF24_SCK_PIN SPI_SCK_PIN
 #define NRF24_MISO_PIN SPI_MISO_PIN
 
-#define FP 1
-#define FM 1
+#define FP 2
+#define FM 2
 #define FG 2
 
 #define HAS_SCREEN 1
-#define ROTATION 0
+#define ROTATION 3
 #define MINBRIGHT 160
 
-#define SDCARD_CS 4
-#define SDCARD_SCK 18
-#define SDCARD_MISO 19
-#define SDCARD_MOSI 23
+#define SDCARD_CS 21
+#define SDCARD_SCK 14
+#define SDCARD_MISO 12
+#define SDCARD_MOSI 13
+
+/* MARAUDER MINI SETTING
+#define SDCARD_SCK SPI_SCK_PIN
+#define SDCARD_MISO SPI_MISO_PIN
+#define SDCARD_MOSI SPI_MOSI_PIN*/
+#define SUPPORT_TRANSACTIONS
 
 #define GROVE_SDA 33
-#define GROVE_SCL 26
+#define GROVE_SCL 25
 
 #define SPI_SCK_PIN 18
 #define SPI_MISO_PIN 19
