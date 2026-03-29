09bdfd37ccb2cc3c3f8fe0917214e3255cec4530
igniskarya <karyaignis@gmail.com>
2026-03-30 06:56:05 +0700

    Update pin definitions and RGB brightness value

--- boards/ESP32-C5-tft/pins_arduino.h
+++ boards/ESP32-C5-tft/pins_arduino.h
@@ -11,7 +11,7 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
 #define LED_BUILTIN LED_BUILTIN // allow testing #ifdef LED_BUILTIN
 // RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
 #define RGB_BUILTIN LED_BUILTIN
-#define RGB_BRIGHTNESS 64
+#define RGB_BRIGHTNESS 10
 
 static const uint8_t TX = 11;
 static const uint8_t RX = 12;
@@ -22,10 +22,10 @@ static const uint8_t USB_DP = 14;
 static const uint8_t SDA = 4;
 static const uint8_t SCL = 5;
 
-static const uint8_t SS = 10;
-static const uint8_t MOSI = 7;
-static const uint8_t MISO = 2;
-static const uint8_t SCK = 6;
+static const uint8_t SS = 6;
+static const uint8_t MOSI = 8;
+static const uint8_t MISO = 9;
+static const uint8_t SCK = 10;
 
 static const uint8_t A0 = 1;
 static const uint8_t A1 = 2;
@@ -57,43 +57,29 @@ static const uint8_t LP_TX = 11;
 // UART
 #define SERIAL_TX 11
 #define SERIAL_RX 12
+
 // I2C
 #define GROVE_SDA 4
 #define GROVE_SCL 5
+
 // SPI
-#define SPI_SCK_PIN 6
-#define SPI_MOSI_PIN 7
-#define SPI_MISO_PIN 2
-#define SPI_SS_PIN 9
+#define SPI_SCK_PIN 10
+#define SPI_MOSI_PIN 8
+#define SPI_MISO_PIN 9
+#define SPI_SS_PIN 3
 
 /* TFT definitions */
 #define HAS_SCREEN 1
 #define ROTATION 1
-#define MINBRIGHT (uint8_t)1
+#define MINBRIGHT (uint8_t)4
 #define USER_SETUP_LOADED 1
 /* ---------------------   */
-//Setup for ST7789 170x320
+// Setup for ST7789 170x320
 
 #define ST7789_DRIVER 1
 #define TFT_RGB_ORDER 0
-#define TFT_WIDTH 240
-#define TFT_HEIGHT 240
-
-/* ---------------------   */
-// Setup for ST7789 240x320
-
-// #define ST7789_DRIVER=1
-// #define TFT_WIDTH=240
-// #define TFT_HEIGHT=320
-// // #define TFT_INVERSION_ON
-// #define TFT_RGB_ORDER=TFT_BGR
-
-/* ---------------------   */
-// Setup for ILI9341 320x240 (no touch)
-
-/*#define ILI9341_DRIVER 1
+#define TFT_WIDTH 170
 #define TFT_HEIGHT 320
-#define TFT_WIDTH 240*/
 
 /* ---------------------   */
 // Common TFT definitions
@@ -101,11 +87,10 @@ static const uint8_t LP_TX = 11;
 #define TFT_BL 25
 #define TFT_RST -1
 #define TFT_DC 24
-#define TFT_MISO 2 // set to share SPI with other devices
-#define TFT_MOSI 7
-#define TFT_SCLK 6
+#define TFT_MISO SPI_MISO_PIN // set to share SPI with other devices
+#define TFT_MOSI SPI_MOSI_PIN
+#define TFT_SCLK SPI_SCK_PIN
 #define TFT_CS 23
-#define TOUCH_CS 1
 #define SMOOTH_FONT 1
 #define SPI_FREQUENCY 20000000
 #define SPI_READ_FREQUENCY 20000000
@@ -119,24 +104,12 @@ static const uint8_t LP_TX = 11;
 #define GPS_SERIAL_TX 5
 #define GPS_SERIAL_RX 4
 
-#ifdef ILI9341_DRIVER
-// Touch Screen
-#define USE_TFT_eSPI_TOUCH 1 // touchscreen uses same tft spi bus
-#define HAS_TOUCH 1
-#define TOUCH_INT -1
-#define TOUCH_CS 1
-#define BTN_ACT LOW
-#define DEEPSLEEP_WAKEUP_PIN 0
-
-#else
-// Buttons
 #define HAS_3_BUTTONS
 #define SEL_BTN 28
 #define DW_BTN 1
 #define UP_BTN 0
 #define BTN_ACT LOW
 #define DEEPSLEEP_WAKEUP_PIN SEL_BTN
-#endif
 
 // InfraRed
 #define RXLED 26
@@ -144,25 +117,25 @@ static const uint8_t LP_TX = 11;
 #define LED_ON HIGH
 #define LED_OFF LOW
 // SDCard
-#define SDCARD_CS 10
+#define SDCARD_CS 23
 #define SDCARD_SCK SPI_SCK_PIN
 #define SDCARD_MISO SPI_MISO_PIN
 #define SDCARD_MOSI SPI_MOSI_PIN
 // CC1101
-#define CC1101_GDO0_PIN 8
-#define CC1101_SS_PIN 9
+#define CC1101_GDO0_PIN 2
+#define CC1101_SS_PIN 3
 #define CC1101_MOSI_PIN SPI_MOSI_PIN
 #define CC1101_SCK_PIN SPI_SCK_PIN
 #define CC1101_MISO_PIN SPI_MISO_PIN
 // NRF24
-#define NRF24_CE_PIN 8
-#define NRF24_SS_PIN 9
+#define NRF24_CE_PIN 2
+#define NRF24_SS_PIN 3
 #define NRF24_MOSI_PIN SPI_MOSI_PIN
 #define NRF24_SCK_PIN SPI_SCK_PIN
 #define NRF24_MISO_PIN SPI_MISO_PIN
 // Ethernet
-#define W5500_INT_PIN 8
-#define W5500_SS_PIN 9
+#define W5500_INT_PIN 2
+#define W5500_SS_PIN 3
 #define W5500_MOSI_PIN SPI_MOSI_PIN
 #define W5500_SCK_PIN SPI_SCK_PIN
 #define W5500_MISO_PIN SPI_MISO_PIN
